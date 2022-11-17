c8051313bc06c635679c4bdddf42d48aa069b586
srivibhav <cs20b047@iittp.ac.in>
2022-11-18 04:06:43 +0530

    Added the basic client server functionality. Tested the same

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,149 @@
-import 'package:flutter/material.dart';
-import 'package:csim/constants/global_variables.dart';
-import 'package:csim/router.dart';
+import 'dart:io';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  static const String _title = 'CSIM';
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'CSIM',
-      theme: ThemeData(
-        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
-        colorScheme: const ColorScheme.light(
-          primary: GlobalVariables.secondaryColor,
-        ),
-        appBarTheme: const AppBarTheme(
-          elevation: 0,
-          iconTheme: IconThemeData(
-            color: Colors.black,
-          ),
-        ),
-      ),
-      onGenerateRoute: (settings) => generateRoute(settings),
+      title: _title,
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Hello. '),
-        ),
-        body: Column(
-          children: [
-            const Center(
-              child: const Text(
-                'Flutter Demo Home Page',
-              ),
-            ),
-            Builder(builder: (context) {
-             return ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, AuthScreen.routeName)
-                  },
-                  child: Text('Click'),);
-          
-  }),
-          ],
-        ),
+        appBar: AppBar(title: const Text('LOGIN')),
+        body: const MyStatefulWidget(),
       ),
     );
+  }
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
+  @override
+  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  static String serverResponse = 'Server response';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'CSIM',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                )),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Sign in',
+                  style: TextStyle(fontSize: 20),
+                )),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                //forgot password screen
+              },
+              child: const Text('Forgot Password',),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                    //_makeGetRequest();
+                    _makeLoginRequest(nameController.text, passwordController.text);
+                    print(nameController.text);
+                    print(passwordController.text);
+                    print(serverResponse);
+                  },
+                )
+            ),
+            Row(
+              children: <Widget>[
+                const Text('Does not have account?'),
+                TextButton(
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    //signup screen
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ],
+        ));
+  }
+  _makeGetRequest() async {
+    final url = Uri.parse(_localhost()); print(url);
+    http.Response response = await http.get(url.replace(path: 'message')); print(response);print(url);
+    setState(() {
+      serverResponse = response.body;
+    }); print(serverResponse); print("2");
+  }
+
+  _makeLoginRequest(String uname, String pass) async{
+    final url = Uri.parse(_localhost());
+    http.Response response = await http.post(url.replace(path: 'login'),
+      headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+      body: jsonEncode(<String, String>{
+        'username': uname, 'password': pass
+      }),);
+    setState(() {
+      serverResponse = response.body;
+    }); print(serverResponse);
+  }
+
+  String _localhost() {
+    if (Platform.isAndroid)
+      return 'http://10.0.2.2:3000';
+    else // for iOS simulator
+      return 'http://localhost:3000';
   }
 }
